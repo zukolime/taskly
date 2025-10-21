ade57a171d0cddaae5f5557df37bb82f8fdd03cd
zukolime <alena.perminova.1993@gmail.com>
2025-10-21 13:06:18 +0500

    feat: adding new task func

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,11 +1,30 @@
+import { useState } from 'react';
 import './Form.scss';
 
-export const Form = () => {
+export const Form = (props: { createNewToDo: Function }) => {
+  const [text, setText] = useState<string>('');
+
+  const formSubmit = () => {
+    if (text) {
+      props.createNewToDo(text);
+      setText('');
+    }
+  };
+
   return (
     <div className='form-wrapper'>
-      <form action='#'>
+      <form
+        action='#'
+        onSubmit={formSubmit}
+      >
         <label>
-          <input type='text' />
+          <input
+            type='text'
+            value={text}
+            onChange={(e) => {
+              setText(e.target.value);
+            }}
+          />
           <button></button>
         </label>
       </form>
--- src/pages/ToDoListPage.tsx
+++ src/pages/ToDoListPage.tsx
@@ -1,26 +1,42 @@
+import { useState } from 'react';
 import { Form } from '../components/Form/Form';
 import { Header } from '../components/Header/Header';
 import { ToDoList } from '../components/ToDoList/ToDoList';
 import { ToDo } from '../models/todo-item';
 
 export const ToDoListPage = () => {
-  const todos: ToDo[] = [
+  const [todos, setTodos] = useState<ToDo[]>([
     {
       id: 1,
       text: 'Первая задача',
       isDone: false,
     },
     {
-      id: 1,
+      id: 2,
       text: 'Вторая задача',
       isDone: true,
     },
-  ];
+    {
+      id: 3,
+      text: 'Третья задача',
+      isDone: true,
+    },
+  ]);
+
+  const createNewToDo = (text: string) => {
+    const newToDo: ToDo = {
+      id: todos.length,
+      text: text,
+      isDone: false,
+    };
+
+    setTodos([...todos, newToDo]);
+  };
 
   return (
     <>
       <Header />
-      <Form />
+      <Form createNewToDo={createNewToDo} />
       <ToDoList todos={todos} />
     </>
   );
