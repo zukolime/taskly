a5331eed466807795080e176af2bfd59f1baaf96
zukolime <alena.perminova.1993@gmail.com>
2025-10-21 12:33:51 +0500

    moving data to top component

--- src/pages/ToDoListPage.tsx
+++ src/pages/ToDoListPage.tsx
@@ -1,13 +1,27 @@
 import { Form } from '../components/Form/Form';
 import { Header } from '../components/Header/Header';
 import { ToDoList } from '../components/ToDoList/ToDoList';
+import { ToDo } from '../models/todo-item';
 
 export const ToDoListPage = () => {
+  const todos: ToDo[] = [
+    {
+      id: 1,
+      text: 'Первая задача',
+      isDone: false,
+    },
+    {
+      id: 1,
+      text: 'Вторая задача',
+      isDone: true,
+    },
+  ];
+
   return (
     <>
       <Header />
       <Form />
-      <ToDoList />
+      <ToDoList todos={todos} />
     </>
   );
 };
