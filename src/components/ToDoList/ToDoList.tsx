d3bcc9e0a667077fad0b8deaa43395f307b3306a
zukolime <alena.perminova.1993@gmail.com>
2025-10-21 12:32:50 +0500

    feat: add funcs for dynamic adding tasks

--- src/components/ToDoList/ToDoList.tsx
+++ src/components/ToDoList/ToDoList.tsx
@@ -1,22 +1,40 @@
 import { ToDoListItem } from './ToDoListItem/ToDoListItem';
 
+import { ToDo } from '../../models/todo-item';
+
 import './ToDoList.scss';
 
-export const ToDoList = () => {
+export const ToDoList = (props: { todos: ToDo[] }) => {
+  const checkedList = () => {
+    return props.todos
+      .filter((item) => !item.isDone)
+      .map((item, id) => {
+        return (
+          <ToDoListItem
+            toDoItem={item}
+            key={id}
+          />
+        );
+      });
+  };
+
+  const uncheckedList = () => {
+    return props.todos
+      .filter((item) => !item.isDone)
+      .map((item, id) => {
+        return (
+          <ToDoListItem
+            toDoItem={item}
+            key={id}
+          />
+        );
+      });
+  };
+
   return (
     <div className='todo-container'>
-      <ul className='todo-list failed'>
-        <ToDoListItem />
-      </ul>
-      <ul className='todo-list completed'>
-        <li className='todo-list-item__wrapper'>
-          <span>Вторая задача</span>
-          <div className='todo-list-item__buttons'>
-            <button className='btn-trash'></button>
-            <button className='btn-uncheck'></button>
-          </div>
-        </li>
-      </ul>
+      <ul className='todo-list failed'>{checkedList()}</ul>
+      <ul className='todo-list completed'>{uncheckedList()}</ul>
     </div>
   );
 };
