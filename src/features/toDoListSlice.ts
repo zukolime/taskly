2a2a54dc17c269d261b843c0d812e6b199c44256
zukolime <alena.perminova.1993@gmail.com>
2025-10-25 14:26:09 +0500

    chore: add uuid

--- src/features/toDoListSlice.ts
+++ src/features/toDoListSlice.ts
@@ -1,6 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 import type { PayloadAction } from '@reduxjs/toolkit';
 import { ToDo } from '../models/todo-item';
+import { v4 as uuid } from 'uuid';
 
 export interface ToDoState {
   todos: ToDo[];
@@ -16,7 +17,7 @@ export const toDoListSlice = createSlice({
   reducers: {
     createAction: (state, action: PayloadAction<string>) => {
       const newToDo: ToDo = {
-        id: state.todos.length,
+        id: uuid(),
         text: action.payload,
         isDone: false,
       };
