b514fe0f11a98fa75fa689eb351045445535d6ff
zukolime <alena.perminova.1993@gmail.com>
2025-10-26 14:21:08 +0500

    fix: add basename in router

--- src/router.tsx
+++ src/router.tsx
@@ -5,31 +5,34 @@ import { ViewList } from './pages/ViewList';
 import { ViewListItem } from './pages/ViewListItem/ViewListItem';
 import { ToDoListPage } from './pages/ToDoListPage';
 
-export const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    errorElement: <NotFound />,
-    children: [
-      {
-        path: '/',
-        element: <ToDoListPage />,
-      },
+export const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Layout />,
+      errorElement: <NotFound />,
+      children: [
+        {
+          path: '/',
+          element: <ToDoListPage />,
+        },
 
-      {
-        path: '/list',
-        element: <ViewList />,
-      },
+        {
+          path: '/list',
+          element: <ViewList />,
+        },
 
-      {
-        path: '/list/:id',
-        element: <ViewListItem />,
-      },
+        {
+          path: '/list/:id',
+          element: <ViewListItem />,
+        },
 
-      {
-        path: '*',
-        element: <NotFound />,
-      },
-    ],
-  },
-]);
+        {
+          path: '*',
+          element: <NotFound />,
+        },
+      ],
+    },
+  ],
+  { basename: '/taskly' }
+);
